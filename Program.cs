4beb423f95aea2d96dc08ee15dbc76ebb02b7093
Daniil <rychenkov16@yandex.ru>
2024-10-06 19:35:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BlockSchemeHW
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User("Tom123", "Tom", true));
+            users.Add(new User("MiKe_005", "Mike", false));
+            users.Add(new User("Alex__O", "Alex", false));
+            users.Add(new User("AgataSnow", "Agata", true));
+            users.Add(new User("Mr_smith", "Nick", true));
+
+            foreach (User user in users)
+            {
+                Console.WriteLine($"Hello, {user.Name}!");
+                if (!user.IsPremium)
+                    Reklama.ShowAds();
+                Console.WriteLine();
+            }
+
+        }
+
+
+    }
+}
